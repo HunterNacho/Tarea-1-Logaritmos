e6a5b19740ec5e2fd11fb3caeb80c084fa51f792
ekauffmann <exploraeniac@gmail.com>
2015-05-01 17:53:27 -0300

    muchas funciones en insertar. Posibles conflictos

--- src/node/AbstractNode.java
+++ src/node/AbstractNode.java
@@ -38,22 +38,78 @@ public abstract class AbstractNode implements INode {
 	
 	@Override  
 	public void insertar(Rectangle rectangle, boolean shouldReinsert) {
+		RNode next;
 		if(elements.get(0).getNext().isLeaf()) {
-			ArrayList<Double> minOverlaps = new ArrayList<Double>();
-			double minOverlap = Double.MAX_VALUE;
+			ArrayList<RNode> minDeltaRNodes = new ArrayList<RNode>();
+			double minDelta = Double.MAX_VALUE;
 			for(RNode n : elements) {
-				//if ()
-				
+				double overlapIni = n.getRectangle().overlap(this.getRectangles());
+				double overlapFin = n.getRectangle().extendWith(rectangle).overlap(this.getRectangles());
+				double delta = overlapFin - overlapIni; 
+				if(delta <= minDelta) {
+					if(delta == minDelta) { 
+						minDeltaRNodes.add(n);
+					}
+					else {
+						minDeltaRNodes.clear();
+						minDeltaRNodes.add(n);
+						minDelta = delta;
+					}					
+				}							
 			}
+			
+			if(minDeltaRNodes.size() > 1) 
+				next = getMinDeltaArea(minDeltaRNodes, rectangle);	
+			else
+				next = minDeltaRNodes.get(0);	
 			
 		}
 		else {
-			
+			next = getMinDeltaArea(elements, rectangle);
 		}
+		next.getNext().insertar(rectangle, shouldReinsert);
 	}
 	
 	@Override
-	public boolean isLeaf(){
+	public boolean isLeaf() {
 		return false;
+	}
+	
+	public static RNode getMinDeltaArea(ArrayList<RNode> rNodes, Rectangle toInsert) {
+		ArrayList<RNode> minDeltaRNodes = new ArrayList<RNode>();
+		double minDeltaArea = Double.MAX_VALUE;
+		for(RNode n : rNodes) {
+			double areaIni = n.getRectangle().getArea();
+			double areaFin = n.getRectangle().extendWith(toInsert).getArea();
+			double delta = areaFin - areaIni; 
+			if(delta <= minDeltaArea) {
+				if(delta == minDeltaArea) { 
+					minDeltaRNodes.add(n);
+				}
+				else {
+					minDeltaRNodes.clear();
+					minDeltaRNodes.add(n);
+					minDeltaArea = delta;
+				}					
+			}		
+		}
+		
+		RNode minAreaRNode = null;
+		double minArea = Double.MAX_VALUE;
+		for(RNode r : minDeltaRNodes) {
+			if(r.getRectangle().getArea() < minArea) {
+				minAreaRNode = r;
+				minArea = r.getRectangle().getArea();
+			}
+		}
+		
+		return minAreaRNode;		
+	}
+	
+	public ArrayList<Rectangle> getRectangles() {
+		ArrayList<Rectangle> rectChildren = new ArrayList<Rectangle>();
+		for(RNode r : elements)
+			rectChildren.add(r.getRectangle());
+		return rectChildren;
 	}
 }
