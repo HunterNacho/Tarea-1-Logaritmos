e1ee4812e85a9aea92269d115f8b9f3cb3fc889a
Ignacio Cuevas <nacho_boom_7stars@hotmail.com>
2015-05-02 11:37:10 -0300

    Esqueleto de insert en AbstractNode

--- src/node/RootNode.java
+++ src/node/RootNode.java
@@ -1,19 +1,10 @@
 package node;
 
-import geometry.Rectangle;
-
-
 public class RootNode extends AbstractNode {
 
 	@Override
 	protected int getLowerLimit() {
 		return 2;
-	}
-
-	@Override
-	public void insertar(Rectangle rectangle, boolean shouldReinsert) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
