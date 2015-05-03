75d4e2d5671a4c6f24d3ee0c878fa6518a2c6cbb
ekauffmann <exploraeniac@gmail.com>
2015-05-03 18:30:14 -0300

    tests de splitOnlyTree con insercion en distinto orden

--- src/tests/SimpleTest.java
+++ src/tests/SimpleTest.java
@@ -9,9 +9,17 @@ import tree.*;
 public class SimpleTest {
 	public static void main(String[] args) {
 		IRTree tree = new SplitOnlyRTree(3);
-		for (int i = 1; i <= 25; i++) {
-			tree.insertar(new Rectangle(i*20, i*20, i*20 + 15 * Math.pow(-1, i), i*20 + 15));
+		for (int i = 1; i <= 12; i++) {
+			tree.insertar(new Rectangle(i*20, i*20, i*20 + 15, i*20 + 15));
 		}
+		
+		for(int i = 24; i>12; i--) {
+			tree.insertar(new Rectangle(i*20, i*20, i*20 + 15, i*20 + 15));
+		}
+		
+//		for (int i = 1; i <= 24; i++) {
+//			tree.insertar(new Rectangle(i*20, i*20, i*20 + 15, i*20 + 15));
+//		}
 		JFrame window = new JFrame();
 	    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    window.setBounds(0, 0, 600, 600);
